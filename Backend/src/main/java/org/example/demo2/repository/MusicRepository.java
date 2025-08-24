99c63dfa80f637471ba564069a192c608b055cda
Nikita Shablov <n1sha2006@icloud.com>
2025-08-25 00:59:48 +0300

    feat: deleted old folder for backend

--- Backend/src/main/java/org/example/demo2/repository/MusicRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.demo2.repository;
-
-import org.example.demo2.entity.Music;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MusicRepository extends JpaRepository<Music, Long> {
-
-}
--- Backend/src/main/java/org/example/demo2/repository/PlayListRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.demo2.repository;
-
-import org.example.demo2.entity.PlayList;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlayListRepository extends JpaRepository<PlayList, Long> {
-}
